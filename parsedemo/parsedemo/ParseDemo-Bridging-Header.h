eb65c76bef1c75bce194dfcf7634e546f5dfdc6a
Joel Castillo <joel.castillo@nyu.edu>
2014-10-28 18:34:36 -0400

    Added v3, removed Parse

--- parsedemo/parsedemo/ParseDemo-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  ParseDemo-Bridging-Header.h
-//  ParseDemo
-//
-//  Created by Joel Castillo on 10/21/14.
-//  Copyright (c) 2014 Joel Castillo. All rights reserved.
-//
-
-#import <Parse/Parse.h>
